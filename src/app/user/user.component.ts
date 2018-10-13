ef42e76f45020bf39d94b9f9c28252769b6b3194
ArseniiT <arseniit@gmail.com>
2018-10-12 21:16:30 -0400

    made one way binding for user component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
-  templateUrl: './user.component.html'
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss']
 })
-export class UserComponent
+export class UserComponent {
+
+  @Input() userComponent;
+
+}
